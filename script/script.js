44e7a93a02801671fae3b14afc2608b3ca89e9f1
bh10-d <duchieubui25012002@gmail.com>
2021-08-22 20:18:39 +0700

    vien gach dau tien

---
+++ script/script.js
@@ -0,0 +1,5 @@
+let dungml = document.querySelector('button');
+let txt = document.getElementById('dungml');
+dungml.addEventListener('click', ()=>{
+    txt.innerHTML = 'Địt con mẹ mày dũng à :))';
+})
