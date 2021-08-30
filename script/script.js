d252c50664861043d533034ce770626827803747
bh-10d <duchieubui25012002@gmail.com>
2021-08-29 21:35:00 -0400

    header====>90%

--- script/script.js
+++ script/script.js
@@ -7,3 +7,14 @@
 //       x.className = "topnav";
 //     }
 //   }
+
+
+
+
+//dropdown for nav-mobile
+
+// let listnav = document.querySelector(".responsive_nav_list_e");
+function dropdownnav() {
+    document.querySelector(".arrow i").classList.toggle("rotage0");
+    document.querySelector(".responsive__nav--list").classList.toggle("show");
+}
