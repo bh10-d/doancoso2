bd73c4786cacf795f7c138176d665142f405f4c0
bh10-d <duchieubui25012002@gmail.com>
2021-09-08 13:03:53 +0700

    header

--- script/script.js
+++ script/script.js
@@ -1,20 +1,22 @@
-//bars
-// function bars() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//       x.className += " responsive";
-//     } else {
-//       x.className = "topnav";
-//     }
-//   }
 
-
-
+function select() {
+    document.querySelector(".drop").classList.toggle("show");
+}
 
 //dropdown for nav-mobile
-
-// let listnav = document.querySelector(".responsive_nav_list_e");
 function dropdownnav() {
     document.querySelector(".arrow i").classList.toggle("rotage0");
     document.querySelector(".responsive__nav--list").classList.toggle("show");
+}
+
+//login-signup
+function changetosignup() {
+    document.querySelector(".login__block--signup").classList.remove("hidden")
+    document.querySelector(".login__block").classList.add("hidden")
+    document.querySelector("title").innerHTML = "Đăng ký"
+}
+function changetologin() {
+    document.querySelector(".login__block--signup").classList.add("hidden")
+    document.querySelector(".login__block").classList.remove("hidden")
+    document.querySelector("title").innerHTML = "Đăng nhập"
 }
